a279beee231da3961087c9f32a02d9a56d90733a
RICHARD WESLEY BARBOSA FREITAS <wesleyrichard30@gmail.com>
2023-04-07 16:15:06 -0300

    create q3

---
+++ Questão 3/script.js
@@ -0,0 +1,13 @@
+console.log(`
+    a) 1, 3, 5, 7, 9  => números impares
+
+    b) 2, 4, 8, 16, 32, 64, 128  => múltiplos de 2
+
+    c) 0, 1, 4, 9, 16, 25, 36, 64  => quadrados perfeitos
+
+    d) 4, 16, 36, 64, 100  => sequência do número atual + 4 * números impares
+
+    e) 1, 1, 2, 3, 5, 8, 13  => tabela fibonacci
+
+    f) 2,10, 12, 16, 17, 18, 19, 200  => números que começam com a letra D em português
+`);
